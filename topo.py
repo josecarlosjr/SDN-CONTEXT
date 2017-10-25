08699fb7d3ceedbfd2960fbe3866f749b22d0753
josecarlosjr <jose.carlosjr@outlook.com>
2017-10-25 11:38:17 -0300

    Update topo.py

--- topo.py
+++ topo.py
@@ -20,26 +20,25 @@ class test( unittest.TestCase ):
 
     def test( self ):
         #Funções para inicialização do iperf cliente
-        #Cada função representa um host 
+        #Cada função representa um host
         #Falta aumentar o tempo de duração do teste do iperf de cada cliente (-t)
         def iperF2():
-            print h2.cmd('iperf3 -c %s -p 5201 -u -b 20M -i 1 -t 240 &' % h1.IP())
+            print h2.cmd('iperf3 -c %s -p 5201 -u -b 20M -i 1 -t 86400 &' % h1.IP())
             return iperF2
-        
+
         def iperF22():
-            print h22.cmd('iperf3 -c %s -p 5202 -u -b 40M -i 1 -t 240 &' % h11.IP())
+            print h22.cmd('iperf3 -c %s -p 5202 -u -b 40M -i 1 -t 86400 &' % h11.IP())
             return iperF22
 
         def iperF3():
-            print h3.cmd('iperf3 -c %s -p 5203 -u -b 5M -i 1 -t 240 &' % h1.IP())
+            print h3.cmd('iperf3 -c %s -p 5203 -u -b 5M -i 1 -t 86400 &' % h1.IP())
             return iperF3
-        
+
         def iperF33():
-            print h33.cmd('iperf3 -c %s -p 5204 -u -b 5M -i 1 -t 240 &' % h11.IP())
-            return iperF33
-        
+            print h33.cmd('iperf3 -c %s -p 5204 -u -b 5M -i 1 -t 86400 &' % h11.IP())
+
         def iperF4():
-            print h4.cmd('iperf3 -c %s -p 5205 -u -b 20M -i 1 -t 240 & ' % h1.IP())
+            print h4.cmd('iperf3 -c %s -p 5205 -u -b 20M -i 1 -t 86400 & ' % h1.IP())
             return iperF4
 
         #INICIALIZA A REDE COM A FUNÇÃO MININET
@@ -47,8 +46,8 @@ class test( unittest.TestCase ):
         info('\n')
         #Esse print net foi apenas para visualizar o que a variável "net" retorna :P. Não é, de fato, necessário
         print net, '<----- net '
-        #ADICIONA O CONTROLADOR 
-        c0 = net.addController( 'c0', ip='192.168.79.10', port=6633 )
+        #ADICIONA O CONTROLADOR
+        c0 = net.addController( 'c0', ip='192.168.56.101', port=6633 )
         info('Adding Hosts\n\n')
         #CRIA OS HOSTS E SWITCHES
         #Cada host possue um rede diferente e foi necessário adicionar uma rota padrão em cada um deles
@@ -66,23 +65,23 @@ class test( unittest.TestCase ):
         s4 = net.addSwitch( 's4_IFPE' )
         #CRIA OS LINKS ENTRE HOSTS E SWITCHES
         info('Link between devices at 100Mb/s\n\n')
-        net.addLink( h1, s1, cls=TCLink, bw=100 )
-        net.addLink( h11, s1, cls=TCLink, bw=100 )
-        net.addLink( h2, s2, cls=TCLink, bw=100 )
-        net.addLink( h22, s2, cls=TCLink, bw=100 )
-        net.addLink( h3, s3, cls=TCLink, bw=100 )
-        net.addLink( h33, s3, cls=TCLink, bw=100 )
-        net.addLink( h4, s4, cls=TCLink, bw=100 )
-        net.addLink( h44, s4, cls=TCLink, bw=100 )
-        net.addLink( s1, s2, cls=TCLink, bw=100 )
-        net.addLink( s2, s3, cls=TCLink, bw=100 )
-        net.addLink( s1, s4, cls=TCLink, bw=100 )
-        net.addLink( s4, s3, cls=TCLink, bw=100 )
-        
+        net.addLink( h1, s1, cls=TCLink, bw=40 )
+        net.addLink( h11, s1, cls=TCLink, bw=40 )
+        net.addLink( h2, s2, cls=TCLink, bw=40 )
+        net.addLink( h22, s2, cls=TCLink, bw=40 )
+        net.addLink( h3, s3, cls=TCLink, bw=40 )
+        net.addLink( h33, s3, cls=TCLink, bw=40 )
+        net.addLink( h4, s4, cls=TCLink, bw=40 )
+        net.addLink( h44, s4, cls=TCLink, bw=40 )
+        net.addLink( s1, s2, cls=TCLink, bw=40 )
+        net.addLink( s2, s3, cls=TCLink, bw=40 )
+        net.addLink( s1, s4, cls=TCLink, bw=40 )
+        net.addLink( s4, s3, cls=TCLink, bw=40 )
+
         try:
             #INICIA A EMULAÇÃO
             net.start()
-            
+
             #CONTADOR DE 30 SEGUNDOS PARA O PROTOCOLO STP
             info('\nwaiting 30sec\n' )
             info('*** converging ring topology\n\n')
@@ -91,21 +90,28 @@ class test( unittest.TestCase ):
                 c+=1
                 print(c)
                 time.sleep(1)
-            
+
             #LISTA TODOS OS HOSTS E SEUS LINKS
             print (net.hosts)
-            
+
             #INICIA O MODO SERVIDOR NOS HOSTS h1 E h11
             info('\n\n*** Starting Iper3 Server h1 and h11\n\n')
-            print h1.cmd('iperf3 -s -p 5201 &') #h2 
+            print h1.cmd('iperf3 -s -p 5201 &') #h2
+            time.sleep(1)
             print h1.cmd('iperf3 -s -p 5203 &') #h3
+            time.sleep(1)
             print h1.cmd('iperf3 -s -p 5205 &') #h4
+            time.sleep(1)
             print h11.cmd('iperf3 -s -p 5202 &') #h22
+            time.sleep(1)
             print h11.cmd( 'iperf3 -s -p 5204 &') #h33
+            time.sleep(1)
             print h1.cmd('jobs')
-            print h11.cmd ('jobs') 
-            
-            #A EMULAÇÃO INICIA COM O TRÁFEGO DOS HOSTS h2 E h4 
+            time.sleep(1)
+            print h11.cmd ('jobs')
+
+
+            #A EMULAÇÃO INICIA COM O TRÁFEGO DOS HOSTS h2 E h4
             #info('\n\n***Starting iperf3 clients on Host2 and Host4\n\n')
             #iperF2()
             #time.sleep(1)
@@ -113,61 +119,61 @@ class test( unittest.TestCase ):
             #iperF4()
             #time.sleep(1)
             #print h4.cmd('jobs')
-             
+
             #PAUSA DE X SEGUNDOS PARA ADICIONAR GRÁFICOS NO CACTI
             #sleep(10)
-            
+
             #TRATAMENTO DE EXCEÇÕES E MENU DE MANIPULAÇÃO DA TOPOLOGIA
-                    
+
             try:
                 info('Choose the action: \n\n')
-                info('a: Start host h3 - s3_CPOR \n')
-                info('b: Link s2_FUNDAJ <-> s1-POP up\n')
-                info('c: Link s2_FUNDAJ <-> s1_POP down\n')
-                info('d: Link s2_FUNDAJ <-> s3_CPOR up\n')
-                info('e: Link s2_FUNDAJ <-> s3_CPOR down\n')
+                info('a: Link s2_FUNDAJ/s1-POP up\n')
+                info('b: Link s2_FUNDAJ/s1_POP down\n')
+                info('c: Link s2_FUNDAJ/s3_CPOR up\n')
+                info('d: Link s2_FUNDAJ/s3_CPOR down\n')
+                info('e: Start host3 - s3_CPOR \n')
                 info('f: Start host22 - s2_FUNDAJ \n')
-                info('g: Start host33 - s3_CPOR \n')
-                info('h: Host-h2 (s2_FUNDAJ) down\n')
-                info('i: Host-h2  (s2_FUNDAJ) up\n')
-                info('j: Host-h22 (s2_FUNDAJ) down\n')
-                info('l: Host-h33 (s3_CPOR) down\n')
-                info('m: Host-h4 (s4_IFPE) up\n')
+                info('g: Shutdown host22 (s2_FUNDAJ) down')
+                info('h: Start host2  (s2_FUNDAJ) up\n')
+                info('i: Shutdown host2 (s2_FUNDAJ) down\n')
+                info('j: Start host33 - s3_CPOR \n')
+                info('l: Shutdown host33 (s3_CPOR) down\n')
+                info('m: Start host4 (s4_IFPE) up\n')
                 info('n: Exit\n\n')
                 inputKey = ''
                 while inputKey != 'n':
                     inputKey = raw_input('Choose an option (just one letter): ')
                     if inputKey == 'a':
                         time.sleep(1)
-                        iperF3()
-                        info('\njobs from h3\n')
-                        print h3.cmd('jobs')
-                        time.sleep(1)
-                        inputKey=''
-                    elif inputKey == 'b':
-                        time.sleep(1)
                         info('\nlink s2_FUNDAJ / s1_POP UP\n')
                         net.configLinkStatus('s2_FUNDAJ','s1_POP','up')
                         time.sleep(1)
-                        inputKey = ''                    
-                    elif inputKey == 'c':
+                        inputKey = ''
+                    elif inputKey == 'b':
                         time.sleep(1)
                         info('\nlink s2_FUNDAJ / s1_POP DOWN\n')
                         net.configLinkStatus('s2_FUNDAJ','s1_POP','down')
                         time.sleep(1)
                         inputKey = ''
-                    elif inputKey == 'd':
+                    elif inputKey == 'c':
                         time.sleep(1)
                         info('\nlink s2_FUNDAJ / s3_CPOR UP\n')
                         net.configLinkStatus('s2_FUNDAJ','s3_CPOR','up')
                         time.sleep(1)
                         inputKey = ''
-                    elif inputKey == 'e':
+                    elif inputKey == 'd':
                         time.sleep(1)
                         info('\nlink s2_FUNDAJ \ s3_CPOR DOWN\n')
                         net.configLinkStatus('s2_FUNDAJ','s3_CPOR','down')
                         time.sleep(1)
                         inputKey = ''
+                    elif inputKey == 'e':
+                        time.sleep(1)
+                        iperF3()
+                        info('\njobs from h3\n')
+                        print h3.cmd('jobs')
+                        time.sleep(1)
+                        inputKey=''
                     elif inputKey == 'f':
                         time.sleep(1)
                         iperF22()
@@ -178,13 +184,25 @@ class test( unittest.TestCase ):
                         inputKey = ''
                     elif inputKey == 'g':
                         time.sleep(1)
-                        info('\n***Starting h33***\n')
-                        iperF33()
+                        info('\nhost-h22 (s2_FUNDAJ) down\n')
+                        print h22.cmd('killid="$(ps -eopid,cmd | egrep "*-p 5202 -u*" | cut -f1 -d " " | head -n 1)"')
                         time.sleep(1)
-                        print h33.cmd('jobs')
-                        time.sleep(1)                    
+                        print h22.cmd('kill 9 $killid')
+                        time.sleep(1)
+                        print h22.cmd('unset killid')
+                        time.sleep(1)
+                        print h22.cmd('jobs')
+                        time.sleep(1)
                         inputKey = ''
                     elif inputKey == 'h':
+                        time.sleep(1)
+                        info('\nhost-h2 (s2_FUNDAJ) UP\n')
+                        iperF2()
+                        time.sleep(1)
+                        print h2.cmd('jobs')
+                        time.sleep(1)
+                        inputKey = ''
+                    elif inputKey == 'i':
                         time.sleep(1)
                         info('\nhost-h2 (s2_FUNDAJ) DOWN\n')
                         print h2.cmd('killid="$(ps -eopid,cmd | egrep "*-p 5201 -u*" | cut -f1 -d " " | head -n 1)"')
@@ -195,24 +213,12 @@ class test( unittest.TestCase ):
                         time.sleep(1)
                         print h2.cmd('jobs')
                         inputKey = ''
-                    elif inputKey == 'i':
-                        time.sleep(1)
-                        info('\nhost-h2 (s2_FUNDAJ) UP\n')
-                        iperF2()
-                        time.sleep(1)
-                        print h2.cmd('jobs')
-                        time.sleep(1)
-                        inputKey = ''
                     elif inputKey == 'j':
                         time.sleep(1)
-                        info('\nhost-h22 (s2_FUNDAJ) down\n')
-                        print h22.cmd('killid="$(ps -eopid,cmd | egrep "*-p 5202 -u*" | cut -f1 -d " " | head -n 1)"')
+                        info('\n***Starting h33***\n')
+                        iperF33()
                         time.sleep(1)
-                        print h22.cmd('kill 9 $killid')
-                        time.sleep(1)
-                        print h22.cmd('unset killid')
-                        time.sleep(1)
-                        print h22.cmd('jobs')
+                        print h33.cmd('jobs')
                         time.sleep(1)
                         inputKey = ''
                     elif inputKey == 'l':
@@ -241,7 +247,7 @@ class test( unittest.TestCase ):
                     else:
                         time.sleep(1)
                         info('\n\n--Option not recognized, Repeat--\n\n')
-                        inputKey = ''                   
+                        inputKey = ''
             except KeyboardInterrupt:
                 print '\n\nDont use Ctrl+C, Use option "m" for exit\n\n'
                 #continue
@@ -256,6 +262,6 @@ if __name__=='__main__':
     setLogLevel('info')
     #setLogLevel('debug')
     #setLogLevel('output')
-    unittest.main()
+    unittest.main(
     
 
