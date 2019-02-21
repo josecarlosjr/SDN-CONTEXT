3a4d03e9ac5a93e33f16d9dcaed0a584c424924e
josecarlosjr <jose.carlosjr@outlook.com>
2019-02-21 09:51:51 -0300

    Update ryu_multipath_1_3.py

--- ryu_multipath_1_3.py
+++ ryu_multipath_1_3.py
@@ -3,10 +3,8 @@
 from ryu.base import app_manager
 from ryu.controller import mac_to_port, ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, DEAD_DISPATCHER, set_ev_cls
-#from ryu.ofproto import ofproto_v1_3
 from ryu.ofproto import ofproto_v1_4
 from ryu.ofproto import ofproto_v1_4_parser
-#from ryu.ofproto import ofproto_v1_5, ofproto_v1_5_parser
 from ryu.lib.mac import haddr_to_bin
 from ryu.lib.packet import packet, arp, ethernet, ipv4, ipv6, ether_types, icmp
 from ryu.lib import mac, ip, hub
