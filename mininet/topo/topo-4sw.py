78ccff317d6c79e07544d428a9112d757290181b
josecarlosjr <jose.carlosjr@outlook.com>
2019-11-13 09:04:09 -0300

    Update topo-4sw.py

--- mininet/topo/topo-4sw.py
+++ mininet/topo/topo-4sw.py
@@ -23,20 +23,23 @@ class icone( Topo ):
             h3 = self.addHost( 'h3', ip='192.168.3.3/24', mac='00:00:00:00:00:03', defaultRoute='via 192.168.3.3', cpu=.1 )
             h4 = self.addHost( 'h4', ip='192.168.4.4/24', mac='00:00:00:00:00:04', defaultRoute='via 192.168.4.4', cpu=.1 )
 
-            s1 = self.addSwitch( 's1_POP' )
-            s2 = self.addSwitch( 's2_FUNDAJ' )
-            s3 = self.addSwitch( 's3_CPOR' )
-            s4 = self.addSwitch( 's4_IFPE' )
+            s1 = self.addSwitch( 's1_POP', cls=OVSKernelSwitch )
+            s2 = self.addSwitch( 's2_FUNDAJ', cls=OVSKernelSwitch )
+            s3 = self.addSwitch( 's3_CPOR', cls=OVSKernelSwitch )
+            s4 = self.addSwitch( 's4_IFPE', cls=OVSKernelSwitch )
 
-            self.addLink( h1, s1, bw=1000, delay='1.26ms', loss=0)#, use_htb=True )
-            self.addLink( h2, s2, bw=1000, delay='1.05ms', loss=0)#, use_htb=True )
+            #LINK BETWEEN HOSTS AND SWITCHES
+            self.addLink( h1, s1, bw=1000, delay='1ms', loss=0)#, use_htb=True )
+            self.addLink( h2, s2, bw=1000, delay='0.875ms', loss=0)#, use_htb=True )
             #self.addLink( h22, s2, bw=100, delay='1.10ms', loss=0, use_htb=True )
-            self.addLink( h3, s3, bw=1000, delay='1.20ms', loss=0)#, use_htb=True )
-            self.addLink( h4, s4, bw=1000, delay='1.20ms', loss=0)#, use_htb=True )
-            self.addLink( s1, s2, bw=1000, delay='1.00ms', loss=0)#, use_htb=True )
-            self.addLink( s2, s3, bw=1000, delay='1ms', loss=0)#, use_htb=True )
-            self.addLink( s1, s4, bw=1000, delay='1.29ms', loss=0)#, use_htb=True )
-            self.addLink( s3, s4, bw=1000, delay='1.15ms', loss=0)#, use_htb=True )
+            self.addLink( h3, s3, bw=1000, delay='3.426ms', loss=0)#, use_htb=True )
+            self.addLink( h4, s4, bw=1000, delay='1.168ms', loss=0)#, use_htb=True )
+
+            #LINK BETWEEN SWITCHES
+            self.addLink( s1, s2, port1=2, port2=3, bw=1000, delay='6.754ms', loss=0, use_htb=True )
+            self.addLink( s2, s3, port1=2, port2=3, bw=1000, delay='6.214ms', loss=0, use_htb=True )
+            self.addLink( s1, s4, port1=3, port2=2, bw=1000, delay='6.322ms', loss=0, use_htb=True )
+            self.addLink( s3, s4, port1=2, port2=3, bw=1000, delay='6.177ms', loss=0, use_htb=True )
 
 topos = {'icone': ( lambda: icone() ) }
 
